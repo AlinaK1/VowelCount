f5372a8e1d2cbc8476d5b4bd02ab9589cc03b453
PadawansTask <padawanstask@gmail.com>
2018-12-28 12:35:18 +0000

    Initial commit

---
+++ Tests/PublicTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal.Execution;
+
+namespace VowelCount.Public
+{
+    [TestFixture]
+    public class PublicTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            Assert.AreEqual(5, VowelCountTask.GetVowelCount("abracadabra"));
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            Assert.AreEqual(0, VowelCountTask.GetVowelCount("kkkwhfk"));
+        }
+
+        [Test]
+        public void TestCase3()
+        {
+            Assert.AreEqual(3, VowelCountTask.GetVowelCount("aaa"));
+        }
+    }
+}
